79bf53932acfca2c90346d26961977d1650b20ec
Cody Askew <codyaskew9@gmail.com>
2023-03-07 13:20:25 -0700

    first commit

---
+++ src/components/database.js
@@ -0,0 +1,138 @@
+export const Leaderboard = [{
+    name: "Ana Babbit",
+    location: "coding club",
+    score :0,
+    img: "",
+    dt:"3-7-2021"},
+{
+    name: "Hailey Alonen",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Isaac Pasi",
+    location: "coding club",
+    score : 0,
+    img:"",
+    dt:"3-7-2021"
+},
+{
+    name: "Aniyah Romero",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Ava Wheeler",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Ella Stevens",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Cecilia Weaks",
+    location: "coding club",
+    score : 0,
+    img: ""},
+{
+    name: "Mason Gamache",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Alora Montano",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Manish Midthana",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Alessandra Zentella",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Prayun Dandu",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Saina Tuniki",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Akshara Iyer",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Lewis Bossler",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Rowan Larson",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Diya Mahendarkar",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Grayson Roettgen",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"},
+{
+    name: "Natalia Hyneman",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+{
+    name: "Elysium Diaz",
+    location: "coding club",
+    score : 0,
+    img: "",
+    dt:"3-7-2021"
+},
+
+]
