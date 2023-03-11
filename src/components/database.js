5ff65f1749715c0b579615b3e8cb145ae9568d39
Cody Askew <codyaskew9@gmail.com>
2023-03-11 12:58:04 -0700

    updated score

--- src/components/database.js
+++ src/components/database.js
@@ -1,138 +1,138 @@
 export const Leaderboard = [{
     name: "Ana Babbit",
     location: "coding club",
-    score :0,
+    score :2,
     img: "",
-    dt:"3-7-2021"},
+    dt:"3-7-2121"},
 {
     name: "Hailey Alonen",
     location: "coding club",
-    score : 0,
+    score : 7,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Isaac Pasi",
     location: "coding club",
-    score : 0,
+    score : 7,
     img:"",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Aniyah Romero",
     location: "coding club",
-    score : 0,
+    score : 1,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Ava Wheeler",
     location: "coding club",
-    score : 0,
+    score : 2,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Ella Stevens",
     location: "coding club",
-    score : 0,
+    score : 9,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Cecilia Weaks",
     location: "coding club",
-    score : 0,
+    score : 2,
     img: ""},
 {
     name: "Mason Gamache",
     location: "coding club",
-    score : 0,
+    score : 2,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Alora Montano",
     location: "coding club",
-    score : 0,
+    score : 10,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Manish Midthana",
     location: "coding club",
-    score : 0,
+    score : 9,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Alessandra Zentella",
     location: "coding club",
-    score : 0,
+    score : 2,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Prayun Dandu",
     location: "coding club",
-    score : 0,
+    score : 1,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Saina Tuniki",
     location: "coding club",
-    score : 0,
+    score : 2,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Akshara Iyer",
     location: "coding club",
-    score : 0,
+    score : 9,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Lewis Bossler",
     location: "coding club",
-    score : 0,
+    score : 2,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Rowan Larson",
     location: "coding club",
-    score : 0,
+    score : 6,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Diya Mahendarkar",
     location: "coding club",
-    score : 0,
+    score : 3,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Grayson Roettgen",
     location: "coding club",
-    score : 0,
+    score : 3,
     img: "",
-    dt:"3-7-2021"},
+    dt:"3-7-2121"},
 {
     name: "Natalia Hyneman",
     location: "coding club",
-    score : 0,
+    score : 2,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 {
     name: "Elysium Diaz",
     location: "coding club",
-    score : 0,
+    score : 7,
     img: "",
-    dt:"3-7-2021"
+    dt:"3-7-2121"
 },
 
 ]
